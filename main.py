06ac76a87c63d28c6dba8d490d776085fc5d2a1a
Russell <russellcorbett11@gmail.com>
2021-07-20 11:28:05 -0230

    Final Room Directions

--- main.py
+++ main.py
@@ -13,7 +13,13 @@ r.rotate_clockwise(90)
 r.forward(700)
 r.rotate_counterclockwise(90)
 r.forward(900)
-r.rotate
+r.rotate_clockwise(30)
+r.forward(575)
+if r.read_marker() == 1:
+    r.rotate_clockwise(60)
+    r.forward(350)
+    r.rotate_counterclockwise(90)
+    r.forward(250)
 
 r.disconnect()
 
