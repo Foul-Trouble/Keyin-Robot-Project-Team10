cef75972837d6f70a0c770d809545ed2c98e98ef
Russell <russellcorbett11@gmail.com>
2021-07-20 11:42:43 -0230

    Final Room Complete

--- main.py
+++ main.py
@@ -20,6 +20,28 @@ if r.read_marker() == 1:
     r.forward(350)
     r.rotate_counterclockwise(90)
     r.forward(250)
+    if r.scan_for_people():
+        r.rescue_person()
+        r.rotate_counterclockwise(180)
+        r.forward(250)
+        r.rotate_clockwise(90)
+        r.forward(350)
+        r.rotate_counterclockwise(60)
+        r.forward(575)
+        r.rotate_counterclockwise(30)
+        r.forward(900)
+        r.rotate_clockwise(90)
+        r.forward(700)
+        r.rotate_counterclockwise(90)
+        r.forward(900)
+        r.rotate_clockwise(90)
+        r.forward(900)
+        r.rotate_clockwise(90)
+        r.forward(2400)
+        r.rotate_clockwise(90)
+        r.forward(1450)
+
+
 
 r.disconnect()
 
