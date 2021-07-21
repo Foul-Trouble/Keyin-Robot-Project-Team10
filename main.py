387dc0cd56b38b7febe7a1224a5a250f188a4881
scottwoolfrey <73621763+scottwoolfrey@users.noreply.github.com>
2021-07-21 11:23:32 -0230

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ r.connect()
 room_5_temp = 0
 
 
+def plot_temp():
+    xs = ["Room 1", "Room 2", "Room 3", "Room 4", "Room 5", "Hall"]
+    ys = [room_1_temp, room_2_temp, room_3_temp, room_4_temp, room_5_temp, hall_temp]
+    plt.plot(xs, ys)
+    plt.show()
+
+
+def fire_loop():
+    Fire = True
+    while Fire:
+        if r.scan_for_fire():
+            r.extinguish_fire()
+        else:
+            Fire = False
+
+
 def room_5():
     r.forward(1450)
     r.rotate_counterclockwise(90)
@@ -84,23 +100,72 @@ def room_5():
         r.forward(1450)
         r.rotate_clockwise(180)
 
+        
+def room1():
+    r.forward(400)
+    r.rotate_counterclockwise(90)
+    mark = r.read_marker()
+    if mark == 1:
+        print(mark)
+        r.forward(790)
+        r.rotate_clockwise(90)
+        r.forward(50)
+        people_scan()
+        global room_1_temp
+        room_1_temp = r.take_temperature()
+        r.backward(50)
+        r.rotate_clockwise(90)
+        r.forward(790)
+        r.rotate_clockwise(90)
+        r.forward(390)
+        r.rotate_clockwise(180)
+        r.forward(950)
+        r.rotate_counterclockwise(90)
+        r.forward(100)
+        return
+    elif mark == 2:
+        r.rotate_clockwise(90)
+        r.forward(500)
+        r.rotate_counterclockwise(90)
+        return
 
-room_5()
 
-r.disconnect()
+def room2():
+    mark = r.read_marker()
+    if mark == 1:
+        print(mark)
+        r.forward(685)
+        r.rotate_clockwise(90)
+        r.forward(50)
+        fire_loop()
+        global room_2_temp
+        room_2_temp = r.take_temperature()
+        r.backward(50)
+        r.rotate_clockwise(90)
+        r.forward(750)
+        r.rotate_counterclockwise(90)
+        r.forward(500)
+        r.rotate_counterclockwise(90)
+        r.forward(1750)
+        r.rotate_counterclockwise(90)
+        r.forward(100)
+        r.rotate_clockwise(45)
+        return
+    if mark == 2:
+        return
 
 
-def plot_temp():
-    xs = ["Room 1", "Room 2", "Room 3", "Room 4", "Room 5", "Hall"]
-    ys = [room_1_temp, room_2_temp, room_3_temp, room_4_temp, room_5_temp, hall_temp]
-    plt.plot(xs, ys)
-    plt.show()
+def room3():
+    m = r.read_marker()
+    print(m)
+    if m == 1:
+        r.rotate_counterclockwise(45)
+        r.forward(200)
+        global room_3_temp
+        room_3_temp = r.take_temperature()
+    else:
+        return
 
+room1()
+room2()
 
-def fire_loop():
-    Fire = True
-    while Fire:
-        if r.scan_for_fire():
-            r.extinguish_fire()
-        else:
-            Fire = False
