f8e987a708829e104a11e95770bae6ea2c2ce2c7
Russell <russellcorbett11@gmail.com>
2021-07-22 10:44:00 -0230

    Kicked

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import robot_mac
 import matplotlib as plt
 r = robot_mac.RobotController()
 r.connect()
+room_1_temp = 0
+room_2_temp = 0
+room_3_temp = 0
+room_4_temp = 0
 room_5_temp = 0
 
 
@@ -21,8 +25,63 @@ def fire_loop():
             Fire = False
 
 
+def hallway(location):
+    global room_1_temp
+    global room_2_temp
+    global room_3_temp
+    global room_4_temp
+    global room_5_temp
+    if location == 1:
+        r.forward(390)
+        r.left(130)
+        if r.read_marker() == 3:
+            #   Go into room 1
+            print("Going into room 1!")
+            r.left(690)
+            if r.scan_for_people:
+                r.rescue_person()
+                room_1_temp = r.take_temperature()
+                r.right(820)
+                r.backward(390)
+            else:
+                room_1_temp = r.take_temperature()
+                r.right(820)
+                r.backward(390)
+        else:
+            #   Go on to room 2
+            r.right(130)
+            r.forward(550)
+            r.left(130)
+            if r.read_marker() == 3:
+                #   Go into room 2
+                print("Going into room 2!")
+                r.left(600)
+                r.forward(75)
+                fire_loop()
+            else:
+                r.right(130)
+                r.forward(510)
+                r.left(1850)
 
 
-room1()
-room2()
+    if location == 2:
+        r.forward(950)
+        r.left(200)
+        mark = r.read_marker()
+        print(mark)
+        if mark == 1:
+            r.left(600)
+            r.forward(150)
+            global room_2_temp
+            room_2_temp = r.take_temperature()
+            fire_loop()
+            r.backward(150)
+            r.right(600)
+        else:
+            r.right(200)
+            r.forward(500)
+            r.left
+            
 
+
+hallway(1)
